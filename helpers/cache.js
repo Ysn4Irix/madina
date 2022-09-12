940bc8ac2f3a5c2fa3ee88d972331ff92607df4e
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2022-09-13 00:38:10 +0100

    🔥 decrease cache ttl

--- helpers/cache.js
+++ helpers/cache.js
@@ -1,6 +1,6 @@
 const NodeCache = require('node-cache')
 
-const Cache = new NodeCache({ stdTTL: 10519200, checkperiod: 120 })
+const Cache = new NodeCache({ stdTTL: 3600, checkperiod: 120 })
 
 module.exports = {
 	setCache: (key, value) => {
